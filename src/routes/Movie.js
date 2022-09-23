12f63b90aa757e6d37928a45080c1968b186a8d0
장성민 <aristatait@gmail.com>
2022-09-23 15:39:27 +0900

    feat: 상세 보기

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,5 +1,37 @@
+import { gql, useQuery } from "@apollo/client";
+import { Link, useParams } from "react-router-dom";
+
+const GET_MOVIE = gql`
+  query getMovie($movieId: String!) {
+    movie(id: $movieId) {
+      id
+      title
+    }
+  }
+`;
+
 const Movie = () => {
-  return <div>여기는 영화 상세보기 페이지 입니다</div>;
+  const params = useParams();
+  console.log("params =", params);
+
+  const { data, loading } = useQuery(GET_MOVIE, {
+    variables: {
+      movieId: params.id
+    }
+  });
+
+  console.log("isLoading =", loading);
+  console.log("data =", data);
+
+  if (loading) return <h1>Movie data fetching...</h1>;
+
+  return (
+    <>
+      <div>여기는 영화 상세보기 페이지 입니다</div>
+      <h1>{data.movie.title}</h1>
+      <Link to={"/"}>목록으로 돌아가기</Link>
+    </>
+  );
 };
 
 export default Movie;
