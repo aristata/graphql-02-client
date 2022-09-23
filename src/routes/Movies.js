df5a915c4a20450218232e7188e0f03d4b28233c
장성민 <aristatait@gmail.com>
2022-09-23 13:52:47 +0900

    feat: useApolloClient

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,5 +1,36 @@
+import { gql, useApolloClient } from "@apollo/client";
+import { useEffect, useState } from "react";
+
 const Movies = () => {
-  return <div>여기는 영화 목록보기 페이지 입니다</div>;
+  // useApolloClient hook
+  const client = useApolloClient();
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    client
+      .query({
+        query: gql`
+          {
+            allMovies {
+              id
+              title
+            }
+          }
+        `
+      })
+      .then((response) => setMovies(response.data.allMovies));
+  }, [client]);
+  return (
+    <>
+      <div>여기는 영화 목록보기 페이지 입니다</div>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default Movies;
