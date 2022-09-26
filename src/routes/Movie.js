98a95d6ba554712eba79efa19d258e5fda6ebd68
장성민 <aristatait@gmail.com>
2022-09-26 14:17:40 +0900

    feat: writeFragment

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -54,10 +54,16 @@ const Image = styled.div`
 `;
 
 const Movie = () => {
+  // react router dom
+  // Movies 컴포넌트에서 Link 를 통해 페이지를 옮길 때 함께 전달한 params 를 가져오는 훅
   const params = useParams();
   console.log("params =", params);
 
-  const { data, loading } = useQuery(GET_MOVIE, {
+  const {
+    data,
+    loading,
+    client: { cache }
+  } = useQuery(GET_MOVIE, {
     variables: {
       movieId: params.id
     }
@@ -68,12 +74,31 @@ const Movie = () => {
 
   if (loading) return <h1>Movie data fetching...</h1>;
 
+  const onClickHandler = () => {
+    // apollo server 의 cache 의 특정 부분만 수정할 때 writeFragment 를 사용한다
+    // id 를 선언하면, 해당 id 의 cache fragment 를 수정한다
+    // fragment gql 은 문법이기 때문에 아래와 같은 형식으로 사용하면 된다
+    // data 에 입력된 값으로 값이 수정된다
+    cache.writeFragment({
+      id: `Movie:${params.id}`,
+      fragment: gql`
+        fragment MovieFragment on Movie {
+          isLiked
+        }
+      `,
+      data: {
+        isLiked: !data.movie.isLiked
+      }
+    });
+  };
   return (
     <Container>
       <Column>
         <Title>{loading ? "Loading..." : `${data.movie?.title}`}</Title>
         <Subtitle>⭐️ {data?.movie?.rating}</Subtitle>
-        <button>{data?.movie?.isLiked ? "Unlike" : "Like"}</button>
+        <button onClick={onClickHandler}>
+          {data?.movie?.isLiked ? "Unlike" : "Like"}
+        </button>
       </Column>
       <Image bg={data?.movie?.medium_cover_image} />
     </Container>
